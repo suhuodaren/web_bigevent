e5b9a51dc6d34ffbbb370a7c2f4ed08e0740628d
suhuodaren <2141208036@qq.com>
2023-08-13 17:15:27 +0800

    完成登录和注册功能的开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,9 @@
+// 注意:每次调用 $.get() 或 $.post() 或 $.ajax() 的时候
+// 会先调用ajaxPrefilter这个函数
+// 在这个函数中,可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function (options) {
+  //   在发起真正的Ajax请求之前, 统一拼接请求的跟路径;
+  options.url = "http://big-event-api-t.itheima.net" + options.url;
+
+  console.log(options.url);
+});
